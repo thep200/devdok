3da1912cd294376fd052757adca2c4d98b63e973
thep200 <thep200.work@gmail.com>
2025-11-07 11:17:47 +0700

    add vitess

--- vitess/region_sharding/create_lookup_schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer_lookup (
-  id int NOT NULL,
-  keyspace_id varbinary(128),
-  primary key(id)
-  );
--- vitess/region_sharding/create_main_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS customer (
-  id int NOT NULL,
-  fullname varchar(256),
-  nationalid varchar(256),
-  country varchar(256),
-  primary key(id)
-  );
